8ee48ed746a051232ffb0ec1e73d78ec9d6bef0b
med-khalil-jabri <mouhamed.khalil.jabri@gmail.com>
2022-08-06 15:51:14 +0930

    train data augmentation

--- src/data_module.py
+++ src/data_module.py
@@ -1,13 +1,17 @@
 import os
+import random
 import sqlite3
 import torch
 import pandas as pd
 import pytorch_lightning as pl
-import torchvision.transforms as transforms
+import torchvision.transforms as T
 from PIL import Image
 from torch.utils.data import Dataset, DataLoader
 from sklearn.model_selection import train_test_split
 from src.dataset_norms import dataset_norms
+
+IMAGE_HEIGHT = 256
+IMAGE_WIDTH = 256
 
 
 class VesselDataset(Dataset):
@@ -33,15 +37,20 @@ class VesselDataset(Dataset):
 class VesselDataModule(pl.LightningDataModule):
     def __init__(self, args):
         self.args = args
-        self.train_transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize(dataset_norms['imagenet21k']['mean'], dataset_norms[('imagenet21k')]['std']),
-            transforms.RandomHorizontalFlip(),
-
+        self.train_transform = T.Compose([
+            T.ToTensor(),
+            T.Normalize(dataset_norms['imagenet21k']['mean'], dataset_norms[('imagenet21k')]['std']),
+            T.RandomHorizontalFlip(p=0.5),
+            T.RandomApply([T.RandomRotation(15)], p=0.25),
+            T.RandomPerspective(p=0.5),
+            T.RandomApply([T.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5))], p=0.25),
+            T.RandomAdjustSharpness(sharpness_factor=2, p=0.25),
+            T.RandomAutocontrast(p=0.25),
+            T.Resize(size=(IMAGE_HEIGHT, IMAGE_WIDTH))
         ])
-        self.test_transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize(dataset_norms['imagenet21k']['mean'], dataset_norms[('imagenet21k')]['std']),
+        self.test_transform = T.Compose([
+            T.ToTensor(),
+            T.Normalize(dataset_norms['imagenet21k']['mean'], dataset_norms[('imagenet21k')]['std']),
         ])
         self.prepare_data_per_node = False
         self.save_hyperparameters(args)
@@ -52,11 +61,14 @@ class VesselDataModule(pl.LightningDataModule):
         df['label'] = pd.Categorical(df.category).codes
         data_split = pd.read_csv(self.args.data_splits, header=None, names=['id', 'set'], skipinitialspace=True)
         df = df[~(df.IMO == '')]
+        df = df[df['label'].isin([80 ,45 ,70 ,35 ,134])]
+        df = df[~df['IMO'].isin([7397464, 7814101, 8128602, 1006245, 8404991, 5273339, 9378840, 7700180, 9159933, 9546497, 8814275, 8431645])]
         train_df = df[df.id.isin(data_split[data_split.set == 'TRAIN'].id)]
         self.train_df, self.val_df = train_test_split(train_df, test_size=0.2, stratify=train_df.IMO)
         self.test_seen_df = df[df.id.isin(data_split[data_split.set == 'PROBE'].id)]
         self.test_unseen_df = df[df.id.isin(data_split[data_split.set == 'TEST'].id)]
         self.train_ds = VesselDataset(self.args.data_dir, self.train_df, self.train_transform)
+        self.train_ds_eval = VesselDataset(self.args.data_dir, self.train_df, self.test_transform)
         self.val_ds = VesselDataset(self.args.data_dir, self.val_df, self.test_transform)
         self.test_seen_ds = VesselDataset(self.args.data_dir, self.test_seen_df, self.test_transform)
         self.test_unseen_ds = VesselDataset(self.args.data_dir, self.test_unseen_df, self.test_transform)
